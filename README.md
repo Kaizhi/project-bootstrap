d14b4df20eb4114a6d083f4c0aa950bd657ae651
Kaizhi Wei <kaizhi@yahoo-inc.com>
2014-11-02 15:20:18 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+#Bootstrap for a stack I'll probably end up using again later.
+
+##Backend
+Express
+Mongoose
+
+##Frontend
+React
+Backbone
+Underscore
+JQuery
+Browserify
+
+
+##Usage
+npm install
+bower install
+grunt watch
+node app
---
+++ public/js/modules/main.js
@@ -0,0 +1,20 @@
+/** @jsx React.DOM */
+var React   = require('react');
+var Backbone = require('backbone');
+var _ = require('underscore');
+Backbone.$ = window.$ = require('jquery'); 
+
+var About = require('../react_components/About');
+
+Backbone.history.start();
+
+React.renderComponent(
+<About docLocation='js/docs/about_my_app.md'/>,
+	document.querySelector('.about_my_app')
+);
+
+React.renderComponent(
+<About docLocation='js/docs/about_how_to.md'/>,
+	document.querySelector('.about_how_to')
+);
+
